e44b9a906143ddb5b1623794d0c0e2b81800222f
MasterTj <798750509@qq.com>
2019-01-12 13:56:07 +0800

    Spring Transaction.... 2018/1/12 13:55

--- spring-Transaction/src/main/java/com/hust/edu/mapper/AirPortMapper.java
+++ spring-Transaction/src/main/java/com/hust/edu/mapper/AirPortMapper.java
@@ -2,7 +2,6 @@ package com.hust.edu.mapper;
 
 import com.hust.edu.pojo.Airport;
 import org.apache.ibatis.annotations.*;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -10,7 +9,6 @@ import java.util.List;
  * locate com.hust.edu.mapper
  * Created by MasterTj on 2019/1/10.
  */
-@Repository
 @Mapper
 public interface AirPortMapper {
     @Select("select * from airport")
